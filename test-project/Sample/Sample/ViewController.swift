cd7ac47e4e4a2ff1b1ac359594ad258c811c8199
Yasuharu Yanamura <yanamura3@gmail.com>
2020-05-04 11:16:15 +0900

    add sample project

---
+++ test-project/Sample/Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by yasuharu.yanamura on 2020/05/04.
+//  Copyright © 2020 yasuharu.yanamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
