834f3cd473d26ddff3da534ca3136354794de6a9
Yasuharu Yanamura <yanamura3@gmail.com>
2020-05-05 14:03:30 +0900

    fix

--- dist/index.js
+++ dist/index.js
@@ -966,11 +966,9 @@ const buildNumber = core.getInput('build-number')
 if (version) {
     const command = `agvtool new-marketing-version ${version}`
     console.log(command)
-    let exitCode = 0
-    exitCode = exec.exec(command)
-    if (exitCode != 0) {
-        core.setFailed(`${command} fail with exit code: ${exitCode}`)
-    }
+    exec.exec(command).catch(err => {
+        core.setFailed(error.message)
+    })
 }
 
 if (!buildNumber) {
