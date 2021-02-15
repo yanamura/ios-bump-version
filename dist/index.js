3b20b2d6191539bf13590ed33c25efce9f84e8c7
Nicola Reggiani <nicola.reggiani@reiteco.com>
2021-02-15 21:46:28 +0100

    Moved project path into exec command

--- dist/index.js
+++ dist/index.js
@@ -19,7 +19,13 @@ module.exports =
 /******/ 		};
 /******/
 /******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete installedModules[moduleId];
+/******/ 		}
 /******/
 /******/ 		// Flag the module as loaded
 /******/ 		module.l = true;
@@ -947,10 +953,72 @@ class ExecState extends events.EventEmitter {
 
 /***/ }),
 
+/***/ 82:
+/***/ (function(__unusedmodule, exports) {
+
+"use strict";
+
+// We use any as a valid input type
+/* eslint-disable @typescript-eslint/no-explicit-any */
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Sanitizes an input into a string so it can be passed into issueCommand safely
+ * @param input input to sanitize into a string
+ */
+function toCommandValue(input) {
+    if (input === null || input === undefined) {
+        return '';
+    }
+    else if (typeof input === 'string' || input instanceof String) {
+        return input;
+    }
+    return JSON.stringify(input);
+}
+exports.toCommandValue = toCommandValue;
+//# sourceMappingURL=utils.js.map
+
+/***/ }),
+
 /***/ 87:
 /***/ (function(module) {
 
 module.exports = require("os");
+
+/***/ }),
+
+/***/ 102:
+/***/ (function(__unusedmodule, exports, __webpack_require__) {
+
+"use strict";
+
+// For internal use, subject to change.
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// We use any as a valid input type
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const fs = __importStar(__webpack_require__(747));
+const os = __importStar(__webpack_require__(87));
+const utils_1 = __webpack_require__(82);
+function issueCommand(command, message) {
+    const filePath = process.env[`GITHUB_${command}`];
+    if (!filePath) {
+        throw new Error(`Unable to find environment variable for file command ${command}`);
+    }
+    if (!fs.existsSync(filePath)) {
+        throw new Error(`Missing file at path: ${filePath}`);
+    }
+    fs.appendFileSync(filePath, `${utils_1.toCommandValue(message)}${os.EOL}`, {
+        encoding: 'utf8'
+    });
+}
+exports.issueCommand = issueCommand;
+//# sourceMappingURL=file-command.js.map
 
 /***/ }),
 
@@ -962,11 +1030,16 @@ const exec = __webpack_require__(986)
 
 const version = core.getInput('version')
 const buildNumber = core.getInput('build-number')
+const projectPath = core.getInput('project-path')
+
+function execCommand(command) {
+  return exec.exec(command, [], {cwd: projectPath})
+}
 
 if (version) {
     const command = `agvtool new-marketing-version ${version}`
     console.log(command)
-    exec.exec(command).catch(error => {
+    execCommand(command).catch(error => {
         core.setFailed(error.message)
     })
 }
@@ -974,16 +1047,18 @@ if (version) {
 if (!buildNumber) {
     const command = `agvtool next-version -all`
     console.log(command)
-    exec.exec(command).catch(error => {
+    execCommand(command).catch(error => {
         core.setFailed(error.message)
     })
 } else {
     const command = `agvtool new-version -all ${buildNumber}`
     console.log(command)
-    exec.exec(command).catch(error => {
+    execCommand(command).catch(error => {
         core.setFailed(error.message)
     })
 }
+
+
 
 /***/ }),
 
@@ -1015,6 +1090,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const os = __importStar(__webpack_require__(87));
+const utils_1 = __webpack_require__(82);
 /**
  * Commands
  *
@@ -1068,28 +1144,14 @@ class Command {
         return cmdStr;
     }
 }
-/**
- * Sanitizes an input into a string so it can be passed into issueCommand safely
- * @param input input to sanitize into a string
- */
-function toCommandValue(input) {
-    if (input === null || input === undefined) {
-        return '';
-    }
-    else if (typeof input === 'string' || input instanceof String) {
-        return input;
-    }
-    return JSON.stringify(input);
-}
-exports.toCommandValue = toCommandValue;
 function escapeData(s) {
-    return toCommandValue(s)
+    return utils_1.toCommandValue(s)
         .replace(/%/g, '%25')
         .replace(/\r/g, '%0D')
         .replace(/\n/g, '%0A');
 }
 function escapeProperty(s) {
-    return toCommandValue(s)
+    return utils_1.toCommandValue(s)
         .replace(/%/g, '%25')
         .replace(/\r/g, '%0D')
         .replace(/\n/g, '%0A')
@@ -1123,6 +1185,8 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const command_1 = __webpack_require__(431);
+const file_command_1 = __webpack_require__(102);
+const utils_1 = __webpack_require__(82);
 const os = __importStar(__webpack_require__(87));
 const path = __importStar(__webpack_require__(622));
 /**
@@ -1149,9 +1213,17 @@ var ExitCode;
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function exportVariable(name, val) {
-    const convertedVal = command_1.toCommandValue(val);
+    const convertedVal = utils_1.toCommandValue(val);
     process.env[name] = convertedVal;
-    command_1.issueCommand('set-env', { name }, convertedVal);
+    const filePath = process.env['GITHUB_ENV'] || '';
+    if (filePath) {
+        const delimiter = '_GitHubActionsFileCommandDelimeter_';
+        const commandValue = `${name}<<${delimiter}${os.EOL}${convertedVal}${os.EOL}${delimiter}`;
+        file_command_1.issueCommand('ENV', commandValue);
+    }
+    else {
+        command_1.issueCommand('set-env', { name }, convertedVal);
+    }
 }
 exports.exportVariable = exportVariable;
 /**
@@ -1167,7 +1239,13 @@ exports.setSecret = setSecret;
  * @param inputPath
  */
 function addPath(inputPath) {
-    command_1.issueCommand('add-path', {}, inputPath);
+    const filePath = process.env['GITHUB_PATH'] || '';
+    if (filePath) {
+        file_command_1.issueCommand('PATH', inputPath);
+    }
+    else {
+        command_1.issueCommand('add-path', {}, inputPath);
+    }
     process.env['PATH'] = `${inputPath}${path.delimiter}${process.env['PATH']}`;
 }
 exports.addPath = addPath;
--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const version = core.getInput('version')
 const buildNumber = core.getInput('build-number')
 const projectPath = core.getInput('project-path')
 
+function execCommand(command) {
+  return exec.exec(command, [], {cwd: projectPath})
+}
+
 if (version) {
     const command = `agvtool new-marketing-version ${version}`
     console.log(command)
-    exec.exec(command).catch(error => {
+    execCommand(command).catch(error => {
         core.setFailed(error.message)
     })
 }
@@ -16,21 +20,13 @@ if (version) {
 if (!buildNumber) {
     const command = `agvtool next-version -all`
     console.log(command)
-    exec.exec(command).catch(error => {
+    execCommand(command).catch(error => {
         core.setFailed(error.message)
     })
 } else {
     const command = `agvtool new-version -all ${buildNumber}`
     console.log(command)
-    exec.exec(command).catch(error => {
-        core.setFailed(error.message)
-    })
-}
-
-if (projectPath) {
-    const command = `cd ${projectPath}`
-    console.log(command)
-    exec.exec(command).catch(error => {
+    execCommand(command).catch(error => {
         core.setFailed(error.message)
     })
 }
